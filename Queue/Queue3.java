04a3f23f845984f78be46db72e97846074da4927
Vittorio Panaia <vittopan19@gmail.com>
2022-12-20 09:25:06 +0800

    List1

--- Queue/Queue3.java
+++ Queue/Queue3.java
@@ -1,60 +1,67 @@
 
 import java.util.NoSuchElementException;
+
 /*
  * This program shows a linked list implementation.
  */
 public class Queue3 {
     private ListNode front;
-    private ListNode rear ;
+    private ListNode rear;
     private int length;
 
-    private class ListNode{
+    private class ListNode {
         private int data;
         private ListNode next;
 
-        public ListNode(int data){
+        public ListNode(int data) {
             this.data = data;
             this.next = null;
         }
     }
-    public void enQueue(int data){
+
+    public void enQueue(int data) {
         ListNode temp = new ListNode(data);
-        if(isEmpty()){
+        if (isEmpty()) {
             front = temp;
-        }else{
+        } else {
             rear.next = temp;
         }
         rear = temp;
         length++;
     }
-    public int deQueue(){
-        if(isEmpty()){
+
+    public int deQueue() throws NoSuchElementException {
+        if (isEmpty()) {
             throw new NoSuchElementException();
         }
         int result = front.data;
         front = front.next;
-        if(front == null){
+        if (front == null) {
             rear = null;
         }
         length--;
         return result;
     }
 
-    public int length(){
+    public int length() {
         return length;
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return length() == 0;
     }
-    public int peek(){
+
+    public int peek() {
         return front.data;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Queue3 a = new Queue3();
         a.enQueue(10);
         a.enQueue(12);
         a.enQueue(17);
-        a.peek();
+        System.out.println(a.deQueue());
+        System.out.println(a.deQueue());
+        System.out.println(a.deQueue());
     }
 }
