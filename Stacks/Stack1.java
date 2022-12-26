1ba964b673d264b394a19385989271cb39324df2
Vittorio Panaia <vittopan19@gmail.com>
2022-12-26 09:44:42 +0800

    Stack (Array)

--- Stacks/Stack1.java
+++ Stacks/Stack1.java
@@ -1,5 +1,41 @@
 package Stacks;
 
-public class Stack1 {
+import java.util.NoSuchElementException;
 
+public class Stack1 {
+    int[] stack = new int[5];
+    int top = 0;
+
+    public void push(int data) {
+        stack[top] = data;
+        top++;
+    }
+
+    public int pop() {
+        top--;
+        int result = stack[top];
+        stack[top] = 0;
+        return result;
+    }
+
+    public int peek() {
+
+        return 0;
+    }
+
+    public int getSize() {
+        int length = 0;
+        for (int i : stack) {
+            if (i != 0) {
+                length += 1;
+            }
+        }
+        return length;
+    }
+
+    public void show() {
+        for (int i : stack) {
+            System.out.println(i);
+        }
+    }
 }
