2b00e60aaf3c441f835c77b1f1333796b0d5f823
Vittorio Panaia <vittopan19@gmail.com>
2022-12-26 10:29:47 +0800

    Implement Stack using a Queue

---
+++ Stacks/MyStack.java
@@ -0,0 +1,51 @@
+package Stacks;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/* Implement a stack using a queue. */
+class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<Integer>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+    }
+
+    public void pop() {
+        Queue<Integer> temp = new LinkedList<Integer>();
+        int counter = 0;
+        while (!queue.isEmpty()) {
+            temp.add(queue.poll());
+            counter++;
+        }
+        while (counter > 1) {
+            queue.add(temp.poll());
+            counter--;
+        }
+    }
+
+    public int top() {
+        Queue<Integer> temp = new LinkedList<Integer>();
+        int counter = 0;
+        while (!queue.isEmpty()) {
+            temp.add(queue.poll());
+            counter++;
+        }
+        while (counter > 1) {
+            queue.add(temp.poll());
+            counter--;
+        }
+        int topInteger = temp.poll();
+        queue.add(topInteger);
+        return topInteger;
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+
+}
--- Stacks/Runner.java
+++ Stacks/Runner.java
@@ -2,12 +2,12 @@ package Stacks;
 
 public class Runner {
     public static void main(String[] args) {
-        Stack1 nums = new Stack1();
-        nums.push(15);
-        nums.push(8);
-        nums.push(69);
-        nums.show();
-        nums.pop();
-        nums.show();
+        MyStack nums = new MyStack();
+        nums.push(5);
+        nums.push(10);
+        nums.push(20);
+        nums.push(3);
+        nums.push(34);
+        System.out.println(nums.pop());
     }
 }
