c2fbe38112684067db9db612fd37fecb6eb8a0e3
Vittorio Panaia <vittopan19@gmail.com>
2023-01-02 11:04:56 +0800

    Queue with Generics

--- Queue/Qgenerics.java
+++ Queue/Qgenerics.java
@@ -1,5 +1,8 @@
 import java.util.NoSuchElementException;
 
+/* 
+ * This Queue implementation was created with generics.
+ */
 class Queue {
     private int size;
     private ListNode front;
@@ -60,8 +63,19 @@ class Queue {
     public static void main(String[] args) {
         Queue q = new Queue();
         Generics<Integer> num1 = new Generics<>(10);
+        Generics<Integer> num2 = new Generics<>(2);
+        Generics<Integer> num3 = new Generics<>(4);
+        Generics<Integer> num4 = new Generics<>(19);
+        Generics<Integer> num5 = new Generics<>(1);
+        Generics<Integer> num6 = new Generics<>(8);
+        Generics<Integer> num7 = new Generics<>(9);
         q.enqueue(num1.ob);
-        q.show();
+        q.enqueue(num2.ob);
+        q.enqueue(num3.ob);
+        q.enqueue(num4.ob);
+        q.enqueue(num5.ob);
+        q.enqueue(num6.ob);
+        q.enqueue(num7.ob);
     }
 }
 
