4b78e5c00a1ce323abbdaa1ded1312f2060a74b5
Vittorio Panaia <vittopan19@gmail.com>
2023-01-02 10:57:22 +0800

    Queue using generics

---
+++ Sorts/sorting.java
@@ -0,0 +1,20 @@
+package Sorts;
+/*
+ * Learning that mergeSort is two seperate sub routines.
+ * Both "Merge" and "Merge Sort"
+ */
+
+public class sorting {
+    public void merge(int[] left_arr, int[] right_arr, int left_size, int right_size) {
+        int i = 0, l = 0, r = 0;
+        // ? The while loop checks the condition for merging.
+        while (l < left_size && r < right_size) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
